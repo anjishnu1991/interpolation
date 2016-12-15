d4c456fb43a5a43c21b0b4be0a628fd29ef3e4b5
Anjishnu Bandyopadhyay <anjishnu.bandyopadhyay@cern.ch>
2016-12-15 20:06:36 +0100

    test changed

--- roofit-fisher/test.C
+++ roofit-fisher/test.C
@@ -10,22 +10,27 @@ void test(){
 
        
 
-	vector<double> alpha1, alpha2;
-        std::map<int, vector<double>> point;
-        std::map<int, vector<double>> point1;
-	alpha1.push_back(0);
+	vector<double> alpha1, alpha2,alpha3;
+        std::map<int, vector<double>> point,point1,point2;
 	alpha1.push_back(1);
-	alpha2.push_back(1);
-	alpha2.push_back(1.5);
+	alpha1.push_back(1);
+	alpha2.push_back(2);
+	alpha2.push_back(2);
+	alpha3.push_back(3);
+	alpha3.push_back(3);
         point[0] = alpha1;
         point1[0] = alpha2;
+        point2[0] = alpha3;
 
 	RooWorkspace* w = new RooWorkspace("ws");
-	RooAbsReal* g1 = (RooAbsReal*)w->factory("Gaussian:g1(x[-5,5], mu[-3,3], sigma[0.5,1.5])");
-	RooAbsReal* g2 =  (RooAbsReal*)w->factory("Gaussian:g2(x[-5,5], mu[-3,3], sigma[0.5,1.5])");
-        RooRealVar* x = w->var("x");
-        RooRealVar* mu = w->var("mu");
-        RooRealVar* sigma = w->var("sigma");
+        for(int i=1;i<4; ++i){
+	 w->factory(Form("Gaussian::g%d(x[-10,10],mu%d[%d,-5,5],sigma%d[%d, 0, 3])", i, i,i,i,i));;
+        }
+	 x = w->var("x");
+         RooRealVar* mu =(RooRealVar*)w->factory("mu[-5,5]");
+	 mu = w->var("mu");
+         RooRealVar*  sigma =(RooRealVar*)w->factory("sigma[0.1,5]");
+         sigma = w->var("sigma");
 /*	string pdf2Name = g2->GetName();
 	string pdf1Name = g1->GetName();
 	string sqrtF_pdf1_pdf2 = "cexpr::sqrt_pdf1_pdf2('sqrt(" + pdf1Name + "*" + pdf1Name + ")'," + pdf1Name + ")";
@@ -39,27 +44,33 @@ void test(){
 
         FunctionMap FisherMap;
         RooArgList List(*mu,*sigma);  
-	w->Print();
-        FisherMap[point] = g1;
-        FisherMap[point1] = g2; 
-        RooFisher * Fisher = new RooFisher("testFisher", "testFisher", List, FisherMap, *w); 
+	RooPlot* frame = x->frame();
+        pdf1= w->pdf("g1");
+	pdf1->plotOn(frame);
+	FisherMap[point] = pdf1;
+        pdf2 = w->pdf("g2");
+	pdf2->plotOn(frame);
+	FisherMap[point1] = pdf2;
+        pdf3 = w->pdf("g3");
+	pdf3->plotOn(frame);
+	FisherMap[point2] = pdf3;
+	frame->Draw();
+        
+	RooFisher * Fisher = new RooFisher("testFisher", "testFisher", List, FisherMap, *w); 
         cout << "a" << endl; 
-	w->import(List);
         cout << "b" << endl; 
 	w->Print();
         cout << "c" << endl; 
 	Fisher->Print("v");
 
-	RooPlot* frame = x->frame();
+	for (int j=0; j<3; ++j){
+		mu->setVal(.8+j);
+		mu->Print();
+		sigma->setVal(.8+j);
+		sigma->Print();
+//		Fisher->plotOn(frame, RooFit::LineColor(kRed));
+}
 
-	for (i=0; i<2; ++i){
-		mu1->setVal(.8+i);
-		mu1->Print();
-		Fisher->plotOn(frame);
-	}	
-
-	
-		frame->Draw();
 
 
 
